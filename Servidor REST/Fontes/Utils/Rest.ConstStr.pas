594f81e3ff2d3287919de752685220fcd003a47a
Nemi Vieira <d2nemi@gmail.com>
2019-09-27 14:57:05 -0300

    Auto Commit

---
+++ Servidor REST/Fontes/Utils/Rest.ConstStr.pas
@@ -0,0 +1,31 @@
+unit Rest.ConstStr;
+
+interface
+
+Const
+  LN = #13 + #10;
+  _PAR_LIMIT = 'limit';
+  _PAR_OFFSET = 'offset';
+  _PAR_ID = 'id';
+  _PAR_DATA = 'data';
+
+  TXT_NOT_EXIST = 'O Registro %s - %s não esta cadastrado no sistema.';
+  TXT_EXIST = 'O Registro %s - %s já está cadastrado no sistema.';
+  TXT_REGISTRO_ATUALIZADO='Registro atualizado com sucesso';
+  TXT_REGISTRO_INCLUIDO='Registro incluido com sucesso';
+  TXT_REGISTRO_EXCLUIDO='Registro excluido com sucesso';
+
+Type
+  TAcao = (acIndefinido, acSelect, acAdd, acUpdate, acDelete, acCancel);
+  TJSONType = (jsArray, jsObject);
+
+Type
+{$IFDEF HASCODEPAGE}
+  TextJSON = type AnsiString(CP_UTF8); // Codepage for an UTF8 string
+{$ELSE}
+  TextJSON = type AnsiString;
+{$ENDIF}
+
+implementation
+
+end.
